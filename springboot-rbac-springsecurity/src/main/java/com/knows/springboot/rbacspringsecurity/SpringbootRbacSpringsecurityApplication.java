53c492566bbc508bd9db719fb10bffdf9099f62d
jianglei-tianma <18761160921@163.com>
2020-04-09 20:20:46 +0800

    s

---
+++ springboot-rbac-springsecurity/src/main/java/com/knows/springboot/rbacspringsecurity/SpringbootRbacSpringsecurityApplication.java
@@ -0,0 +1,15 @@
+package com.knows.springboot.rbacspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringbootRbacSpringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRbacSpringsecurityApplication.class, args);
+    }
+
+}
---
+++ springboot-rbac-springsecurity/src/test/java/com/knows/springboot/rbacspringsecurity/SpringbootRbacSpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knows.springboot.rbacspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRbacSpringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
